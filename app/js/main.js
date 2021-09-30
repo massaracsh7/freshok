0e441e758a3fb246d351742561cfa1b2a0796304
Your Name <you@example.com>
2021-09-30 10:49:50 +0300

    added banners, stocks, news

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,18 @@ $(function () {
     autoplaySpeed: 3000
   });
 
-  var mixer = mixitup('.product-items');
+
+  var container1 = document.querySelector('[data-ref="mixer-1"]');
+  var container2 = document.querySelector('[data-ref="mixer-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(container1, config);
+  var mixer2 = mixitup(container2, config);
+
 
 });
