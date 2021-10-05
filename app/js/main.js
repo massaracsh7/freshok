c32b4341b5f71bc299cc0550e14fec15e8d6bb6d
Your Name <you@example.com>
2021-10-05 10:53:54 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,19 @@ $(function () {
 
   $('.slider-sale__inner').slick({
     dots: false,
-    arrows: true,
+    arrows: false,
     fade: true,
     autoplay: false,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000,
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        dots: true,
+        arrows: false
+      }
+    }]
   });
+
 
 
   var container1 = document.querySelector('[data-ref="mixer-1"]');
@@ -21,5 +29,16 @@ $(function () {
   var mixer1 = mixitup(container1, config);
   var mixer2 = mixitup(container2, config);
 
+  $('.header__btn').on('click', function () {
+    $('.header__top').toggleClass('header__top--close');
+    $('.main').toggleClass('main--close');
+    $('.mob-nav').toggleClass('mob-nav--active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.form-search__open').on('click', function () {
+    $('.form-search').toggleClass('form-search--active');
+    $('.form-search__text').attr("placeholder", "Я ищу...");
+  });
 
 });
